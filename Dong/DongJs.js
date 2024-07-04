13fb0c351476571c55e6aafd1840a9ec43854749
hyun <qudgus6012@naver.com>
2024-07-04 13:55:12 +0900

    second

--- Dong/DongJs.js
+++ Dong/DongJs.js
@@ -7,9 +7,21 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const toggleCheckbox = (allbox, itembox) => {
         allbox.addEventListener('change', () => {
+            let allViewed = true;
             itembox.forEach((item) => {
-                item.checked = allbox.checked;
+                if (item.dataset.viewed === 'false') {
+                    allViewed = false;
+                }
             });
+
+            if (allViewed) {
+                itembox.forEach((item) => {
+                    item.checked = allbox.checked;
+                });
+            } else {
+                alert('내용 보기를 눌러 약관을 확인해 주세요.');
+                allbox.checked = false;
+            }
         });
     };
 
@@ -18,15 +30,34 @@ document.addEventListener('DOMContentLoaded', () => {
 
     $listInput.forEach((item) => {
         item.addEventListener('change', () => {
-            const isChecked = Array.from($listInput).every(i => i.checked);
+            const isChecked = Array.from($listInput).every(i => i.checked || i.dataset.viewed === 'false');
             $selectall.checked = isChecked;
+        });
+
+        item.addEventListener('click', (event) => {
+            if (item.dataset.viewed === 'false') {
+                alert('내용 보기를 눌러 약관을 확인해 주세요.');
+                event.preventDefault();
+            }
         });
     });
 
     $mktlistinput.forEach((item) => {
         item.addEventListener('change', () => {
-            const isChecked = Array.from($mktlistinput).some(i => i.checked);
+            const isChecked = Array.from($mktlistinput).some(i => i.checked || i.dataset.viewed === 'false');
             $selectallmkt.checked = isChecked;
+        });
+
+        item.addEventListener('click', (event) => {
+            if (item.dataset.viewed === 'false') {
+                alert('내용 보기를 눌러 약관을 확인해 주세요.');
+                event.preventDefault();
+            }
         });
     });
 });
+
+function markAsViewed(link) {
+    const checkbox = link.previousElementSibling.querySelector('input[type="checkbox"]');
+    checkbox.dataset.viewed = 'true';
+}
